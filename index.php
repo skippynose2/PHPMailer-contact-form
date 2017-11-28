1b79c1393e26955b3df57e1ec2da46c549d21b48
Akhil <devarapalli148@gmail.com>
2017-11-27 20:37:23 -0500

    This is final form with all the features included just copy and paste your information in it

--- index.php
+++ index.php
@@ -6,14 +6,34 @@
   </head>
   <body>
     <?php
+    require 'PHPMailer-5.2-stable/PHPMailerAutoload.php';
       function Form()
       {
         if (!empty($_POST['name']) && !empty($_POST['email'])) {
           # code...
           //createing vars for getting info
           $name = htmlentities($_POST['name']); // do not mess with
-          $email = htmlentities($_POST['email']); // do not mess with 
-          if
+          $email = htmlentities($_POST['email']); // do not mess with
+          if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $mail = new PHPMailer();
+            $mail -> isSMTP();
+            $mail -> SMTPAuth = true;
+            $mail -> SMTPSecure = 'ssl';
+            $mail -> Host = 'smtp.gmail.com';
+            $mail->Port = '465';
+            $mail -> isHTML(true);
+            $mail -> Username = "your username here";
+            $mail -> Password = 'your password here';
+            $mail -> setFrom("contactformphp@php.com","Contact form results");
+            $mail -> addAddress("example@example.com");
+            $mail -> Subject = "This is mail from your website";
+            $mail -> Body = "Name is " . " " . $name ." " . "This is email " . $email;
+            $mail -> send();
+            echo "message sent";
+
+          }else{
+            echo "please enter email";
+          }
         }else {
           echo "please enter something";
         }
